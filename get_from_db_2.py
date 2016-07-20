91afa8d2edcca1d12386e0795e6db746be6c477d
Marco Roggemann <marco.roggemann@rl-institut.local>
2016-07-20 14:06:51 +0200

    add more polygons for testing purpose

--- get_from_db_2.py
+++ get_from_db_2.py
@@ -1,4 +1,4 @@
-import shapefile
+#import shapefile
 import oemof.db as db
 from shapely import geometry as geopy
 from oemof.db import coastdat
@@ -42,7 +42,7 @@ conn = db.connection()
 germany = fetch_geometries(**germany)
 germany['geom'] = geoplot.postgis2shapely(germany.geom)
 # print(germany)
-# print(germany['geom'])
+print(germany['geom'])
 geom = geopy.Polygon([(12.2, 52.2), (12.2, 51.6), (13.2, 51.6), (13.2, 52.2)])
 multi_weather = coastdat.get_weather(conn, geom, year)
 my_weather = multi_weather[0]
@@ -51,9 +51,9 @@ print(my_weather.geometry)
 # print(len(multi_weather))
 vector_coll = {}
 
-# Collecting vectors in dictionary
-# Calm less than value
-# El Schleifo
+# Collecting calm vectors in dictionary
+# For loop to find the longest calms per weather object
+
 for i in range(len(multi_weather)):
 
     # print(multi_weather[i].data['v_wind'])
@@ -63,7 +63,7 @@ for i in range(len(multi_weather)):
     vc = vector_coll
     calm = len(max(vc, key=len))
     # multi_weather[i].geometry
-    print(calm)
+    #print(calm)
 
 # Germany dena_18 regions (ZNES)
 
@@ -92,15 +92,20 @@ coastdat_de['geom'] = geoplot.postgis2shapely(coastdat_de.geom)
 
 germany = fetch_geometries(**germany)
 germany['geom'] = geoplot.postgis2shapely(germany.geom)
-print(geom)
-print(coastdat_de['geom'])
+#print(geom)
+#print(coastdat_de['geom'])
 
+# random example polygons
 geom_2 = geopy.Polygon([(14.2, 52.2), (14.2, 51.6), (15.2, 51.6), (15.2, 52.2)])
+geom_3 = geopy.Polygon([(16.2, 52.2), (16.2, 51.6), (17.2, 51.6), (17.2, 52.2)])
+geom_4 = geopy.polygon([(14.2, 53.4), (14.2, 52.8), (15.2, 52.8), (15.2, 53.4)])
+
 # data_example = {geom: 130, geom_2: 2}
-data_example = [130, 2]
+
+data_example = [500, 300, 100, 1]
 # example = geoplot.GeoPlotter(coastdat_de['geom'], (3, 16, 47, 56),
                              # data=np.random.rand(792))
-example = geoplot.GeoPlotter([geom, geom_2], (3, 16, 47, 56),
+example = geoplot.GeoPlotter([geom, geom_2, geom_3, geom_4], (3, 20, 47, 60),
                              data=data_example)
 example.cmapname = 'winter'
 example.cmapname = 'winter'
