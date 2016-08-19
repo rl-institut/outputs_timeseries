d5028f84ffcacb4e184a94e25a24a4d2362a46bc
Marco Roggemann <marco.roggemann@rl-institut.local>
2016-08-19 10:42:28 +0200

    reupload

--- get_from_db_2.py
+++ get_from_db_2.py
@@ -11,6 +11,7 @@ import fiona
 from sklearn import preprocessing
 from matplotlib.colors import LogNorm
 import matplotlib.colors as colors
+from matplotlib.ticker import LogLocator
 
 def fetch_geometries(**kwargs):
     """Reads the geometry and the id of all given tables and writes it to
@@ -42,7 +43,7 @@ germany = {
 
 print('collecting weather objects...')
 #geometrie = shapefile.Reader("~/temp/deutschland.shp")
-year = 2007
+year = 2014
 conn = db.connection()
 germany = fetch_geometries(**germany)
 germany['geom'] = geoplot.postgis2shapely(germany.geom)
@@ -72,19 +73,28 @@ calm_list = []
 print('calculating calms...')
 for i in range(len(multi_weather)):
 
-    calm, = np.where(multi_weather[i].data['v_wind'] < 5)
+    calm, = np.where(multi_weather[i].data['v_wind'] < 3)
     vector_coll = np.split(calm, np.where(np.diff(calm) != 1)[0] + 1)
     vc = vector_coll
     calm = len(max(vc, key=len))
+
 #    multi_weather[i].geometry
     calm_list = np.append(calm_list, calm)
-    calm_list2 = calm_list / calm_list.max(axis=0)
+    calm_list2 = np.log(calm_list) / np.log(calm_list.max(axis=0))
+    calm_list3 = np.sort(calm_list)
 #np.save(calm_list, calm_list)
-print(calm_list)
+print(calm_list3)
 x = np.amax(calm_list)
 y = np.amin(calm_list)
 print(x)
 print(y)
+
+plt.hist(calm_list3, normed=False, range=(calm_list3.min(),
+     calm_list3.max()), log=True)
+plt.xlabel('length of calms in hours')
+plt.ylabel('number of calms')
+plt.title('calm histogram Germany{0}'.format(year))
+plt.show()
 
 #print(multi_weather.data)
 
@@ -126,23 +136,24 @@ x = coastdat_de['geom']
 df = pd.DataFrame(data=calm_list2, columns=['calms'])
 df2 = pd.DataFrame(data=x, columns=['geom'])
 df3 = pd.concat([df, df2], axis=1)
-print(df3)
+#print(df3)
 
 example = geoplot.GeoPlotter(df3['geom'], (3, 16, 47, 56),
                                 data=df3['calms'])
 
-example.cmapname = 'winter'
+example.cmapname = 'inferno'
 #example.cmapname = 'winter'
 #example.plot(facecolor='OrRd', edgecolor='')
 
 #example.geometries = germany['geom'] -> Netzregionen
 #example.data = None
-example.plot(facecolor='inferno', edgecolor='black', linewidth=1, alpha=1)
+example.plot(edgecolor='black', linewidth=1, alpha=1)
 
 print('creating plot...')
 plt.title('Longest calms Germany {0}'.format(year))
-example.draw_legend((63, 8760), legendlabel="Length of wind calms < 5 m/s in h",
-                           extend='neither', norm=colors.LogNorm())
+example.draw_legend(legendlabel="Length of wind calms < 3 m/s in h",
+                     extend='neither', tick_list=[1, 10, 100, 1000, 5172])
+
 example.basemap.drawcountries(color='white', linewidth=2)
 example.basemap.shadedrelief()
 plt.tight_layout()
